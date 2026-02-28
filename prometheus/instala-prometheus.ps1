356cc2ca2a8e520480ef9ca033e782425b43d01c
egsmartin <egsmartin@gmail.com>
2026-02-28 18:43:09 +0100

    .

--- prometheus/instala-prometheus.ps1
+++ prometheus/instala-prometheus.ps1
@@ -5,6 +5,10 @@ kubectl apply -f ./grafana-secret.yaml
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+# Instala la clase de almacenamiento dinamico local-path
+Write-Host "Ensure local-path storage class (if you already have it, this is a no-op)..."
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
 # instala chart de prometheus en el namespace monitoring (se crea si no existe)
 helm install kube-prom-stack prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace -f values.yaml
 
